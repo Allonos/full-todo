eae10a417a6a6db9047bebda7103f27b72118081
Allonos <revaztsitsvidze@icloud.com>
2025-06-05 16:21:25 +0400

    Finished deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import CreatePage from "./pages/CreatePage";
 const App = () => {
   return (
     <div className="relative h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/create" element={<CreatePage />}></Route>
